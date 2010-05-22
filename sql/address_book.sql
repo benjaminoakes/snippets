a2e0625bf656037a81491dcfc16c98c30b1f4511
Ben Oakes <ben@benjaminoakes.com>
2010-05-21 21:08:57 -0400

    SQL (SQLite3).  There's a lot more, but it would take some work to push it up to GitHub.

---
+++ sql/address_book.sql
@@ -0,0 +1,33 @@
+create table people (
+  id integer primary key autoincrement,
+  first_name varchar(255),
+  middle_name varchar(255),
+  last_name varchar(255),
+  birthday date,
+  type varchar(50)
+);
+
+create table addresses (
+  id integer primary key autoincrement,
+  person_id integer,
+  type varchar(50),
+  address varchar(255),
+  city varchar(255),
+  state varchar(50),
+  zip varchar(15)
+);
+
+create table phone_numbers (
+  id integer primary key autoincrement,
+  person_id integer,
+  type varchar(50),
+  number varchar(50)
+);
+
+create table email_addresses (
+  id integer primary key autoincrement,
+  person_id integer,
+  type varchar(50),
+  email_address varchar(255)
+);
+
