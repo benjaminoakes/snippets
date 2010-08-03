7ed75d2bc9b8674bdb675e5407ce117fb41d56d0
Ben Oakes <ben@benjaminoakes.com>
2010-08-03 14:09:42 -0400

    Ruby: RB OSA sandbox

---
+++ ruby/rbosa_sandbox.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'rbosa'
+
+def open_app(name)
+  puts %x[open "/Applications/#{name}.app"]
+end
+
+safari = OSA.app('Safari')
+
+adium = OSA.app('Adium')
+open_app('Gmail')
+
+open_app('Gcal')
+omni_focus = OSA.app('OmniFocus')
+
+open_app('Utilities/Terminal')
+
+text_mate = OSA.app('TextMate')
+
+itunes = OSA.app('iTunes')
+
+# safari = OSA.app('Safari')
+# puts safari.methods.sort
+
