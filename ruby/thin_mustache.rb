fdbd61053b4b2cee5f6ef78db5355ad667e3fa23
Ben Oakes <ben@benjaminoakes.com>
2012-09-21 15:27:00 -0500

    Thin mustache {

---
+++ ruby/thin_mustache.rb
@@ -0,0 +1,28 @@
+# Do you only need to replace some tokens?  This is all you need (Ruby 1.9+).
+module ThinMustache
+  class << self
+    def render(template, doc)
+      template.gsub(/{{.*?}}/, curl(doc))
+    end
+
+    private
+
+    def curl(hash)
+      hash.keys.reduce({}) { |a, e|
+        a["{{#{e}}}"] = hash[e]
+        a
+      }
+    end
+  end
+end
+
+require 'test/unit'
+
+class ThinMustacheTest < Test::Unit::TestCase
+  def test_render
+    template = 'Hi {{name}}, This is a message for you. {{closer}}, {{me}}'
+    doc = { :name => 'Danielle', :closer => 'Love always', :me => 'Ben' }
+
+    assert_equal 'Hi Danielle, This is a message for you. Love always, Ben', ThinMustache.render(template, doc)
+  end
+end
