0a0ad98a8c49f06b99e9ebc76c19303a1f13981e
Ben Oakes <hello@benjaminoakes.com>
2012-09-21 15:32:49 -0500

    (m)

--- ruby/thin_mustache.rb
+++ ruby/thin_mustache.rb
@@ -1,6 +1,6 @@
-#     ___________________^___________________
-#    /                                       \
-#    |                                       |
+#     ___________________/\___________________
+#    /                                        \
+#    |                                        |
 #
 # Do you only need to replace some tokens?  This is all you need (Ruby 1.9+).
 module ThinMustache
