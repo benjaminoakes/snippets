d973c33f99939f88d80e2f5d5c4b12348ec97bfd
Ben Oakes <hello@benjaminoakes.com>
2013-03-09 14:49:31 -0600

    Update to_h.rb

--- ruby/to_h.rb
+++ ruby/to_h.rb
@@ -6,6 +6,8 @@ class Object
   # Collect the result of the methods into a hash.
   #
   # NOTE: Ruby 2.0 introduced using a method with the same name; be careful if using this with Ruby 2.0!
+  #
+  # See my blog post on to_h: http://www.benjaminoakes.com/2013/03/08/all-about-to_h-in-ruby-2/
   def to_h(*methods)
     methods.inject({}) do |hash, method|
       hash[method] = send(method)
